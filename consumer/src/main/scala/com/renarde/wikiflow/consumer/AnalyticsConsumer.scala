1f99595f90a40e23d77e1080dde72628ec2a2c33
vrudakov <vrudakov@list.ru>
2019-10-05 18:45:21 +0300

    Add code

--- consumer/src/main/scala/com/renarde/wikiflow/consumer/AnalyticsConsumer.scala
+++ consumer/src/main/scala/com/renarde/wikiflow/consumer/AnalyticsConsumer.scala
@@ -1,31 +1,77 @@
 package com.renarde.wikiflow.consumer
-
 import com.typesafe.scalalogging.LazyLogging
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
 
 object AnalyticsConsumer extends App with LazyLogging {
-
   val appName: String = "analytics-consumer-example"
 
   val spark: SparkSession = SparkSession.builder()
     .appName(appName)
     .config("spark.driver.memory", "5g")
+    .config("spark.sql.streaming.checkpointLocation", "/storage/analytics-consumer/checkpoints")
     .master("local[2]")
     .getOrCreate()
   spark.sparkContext.setLogLevel("WARN")
-
+  import spark.implicits._
   logger.info("Initializing Structured consumer")
 
-
-  val inputStream = spark.readStream
+  val inputStream: DataFrame = spark.readStream
     .format("kafka")
     .option("kafka.bootstrap.servers", "kafka:9092")
     .option("subscribe", "wikiflow-topic")
     .option("startingOffsets", "earliest")
     .load()
 
-  // please edit the code below
+
+  val expectedSchema = new StructType()
+    .add(StructField("bot", BooleanType))
+    .add(StructField("comment", StringType))
+    .add(StructField("id", LongType))
+    .add("length", new StructType()
+      .add(StructField("new", LongType))
+      .add(StructField("old", LongType))
+    )
+    .add("meta", new StructType()
+      .add(StructField("domain", StringType))
+      .add(StructField("dt", StringType))
+      .add(StructField("id", StringType))
+      .add(StructField("offset", LongType))
+      .add(StructField("partition", LongType))
+      .add(StructField("request_id", StringType))
+      .add(StructField("stream", StringType))
+      .add(StructField("topic", StringType))
+      .add(StructField("uri", StringType))
+    )
+    .add("minor", BooleanType)
+    .add("namespace", LongType)
+    .add("parsedcomment", StringType)
+    .add("patrolled", BooleanType)
+    .add("revision", new StructType()
+      .add("new", LongType)
+      .add("old", LongType)
+    )
+    .add("server_name", StringType)
+    .add("server_script_path", StringType)
+    .add("server_url", StringType)
+    .add("timestamp", LongType)
+    .add("title", StringType)
+    .add("type", StringType)
+    .add("user", StringType)
+    .add("wiki", StringType)
+
+
+
   val transformedStream: DataFrame = inputStream
+    .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)").as[(String, String)]
+    .filter($"value".isNotNull)
+    .select(from_json($"value", expectedSchema).as("data"))
+    .select("data.*")
+    .filter($"bot" =!= true)
+    .groupBy($"type")
+    .count
+    .withColumn("timestamp", current_timestamp())
 
   transformedStream.writeStream
     .outputMode("append")
